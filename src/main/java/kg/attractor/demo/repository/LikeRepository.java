c2f16a01993088f74d09b5f6f96dfe52d921352d
Semma <semm1998>
2020-03-12 17:57:44 +0600

    приципы я хотел поставил на приложении миркорграм поставил пост слушать песю

--- src/main/java/kg/attractor/demo/repository/LikeRepository.java
+++ src/main/java/kg/attractor/demo/repository/LikeRepository.java
@@ -3,9 +3,11 @@ package kg.attractor.demo.repository;
 
 
 
+import kg.attractor.demo.model.Like;
 import kg.attractor.demo.model.LikeModel;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LikeRepository extends CrudRepository<LikeModel, String> {
 
+    void save ( Like like );
 }
