505393df18a8b04f59a7bfc72e36077710fdf30e
Semma <semm1998>
2020-03-12 18:20:12 +0600

    приципы я хотел поставил на приложении миркорграм поставил пост слушать песю

--- src/main/java/kg/attractor/demo/model/ikeRepository.java
+++
@@ -1,8 +0,0 @@
-package kg.attractor.demo.model;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ikeRepository extends MongoRepository<Like, String> {
-    //посчитать все лайки публикации
-    int findAllByPublication ( Publication publication );
-}
