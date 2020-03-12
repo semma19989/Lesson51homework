c2967da4e7d56c12778b3b45deebe7e2423c9927
Semma <semm1998>
2020-03-12 18:01:49 +0600

    приципы я хотел поставил на приложении миркорграм поставил пост слушать песю

---
+++ src/main/java/kg/attractor/demo/util/PreloaderDatabase.java
@@ -0,0 +1,30 @@
+package kg.attractor.demo.util;
+
+
+import kg.attractor.demo.model.User;
+import kg.attractor.demo.repository.UserRepo;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.stream.Stream;
+
+@Configuration
+public class PreloaderDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase ( UserRepo repository ) {
+
+        repository.deleteAll();
+
+        return ( args ) -> Stream.of(users())
+                .peek(System.out::println)
+                .forEach(repository::save);
+    }
+
+    private User[] users () {
+        return new User[]{
+                new User("1", "semma", "semm1998ac@gmail.com", null),
+                new User("2", "syimuk", "semm1998@bk.ru", null)};
+    }
+}
